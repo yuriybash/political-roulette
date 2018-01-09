8a70686025f2f1c3fcfe1022616175839812b50e
Yuriy Bash <yuriybash@gmail.com>
2018-01-09 04:41:31 -0400

    cleans up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Button, PageHeader} from 'reactstrap';
+import {Button} from 'reactstrap';
 import ReactLoading from 'react-loading';
 import {connect} from "./connection";
 
-
-/* START WEBSOCKET-RELATED VARS */
-
-// The media constraints object describes what sort of stream we want
-// to request from the local A/V hardware (typically a webcam and
-// microphone). Here, we specify only that we want both audio and
-// video; however, you can be more specific. It's possible to state
-// that you would prefer (or require) specific resolutions of video,
-// whether to prefer the user-facing or rear-facing camera (if available),
-// and so on.
-//
-// See also:
-// https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamConstraints
-// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-//
-
-// To store username of other peer
-// RTCPeerConnection
-
-
-// To work both with and without addTrack() we need to note
-// if it's available
-
-/*
-END WEBSOCKET-RELATED VARS
- */
-
-
-
-/* RECIPIENT RECEIVING OFFER BELOW */
-
-
-
-
-/* END WSS CODE */
-
-
-
-
-
-/* APP CODE */
 class Video extends React.Component{
 
     hangUpCall(){
@@ -93,9 +52,15 @@ class UserSelection extends React.Component {
 
 class App extends Component {
 
-  on_delay(){
+    state = {
+        selectorIsHidden: false,
+        videoIsHidden: true,
+        delayIsHidden: true
+    };
+
+    on_delay(){
       this.setState({
-          isHidden: true,
+          selectorIsHidden: true,
           videoIsHidden: true,
           delayIsHidden: false
       })
@@ -103,37 +68,21 @@ class App extends Component {
 
   on_call_start(){
       this.setState({
-          isHidden: true,
+          selectorIsHidden: true,
           videoIsHidden: false,
           delayIsHidden: true
       })
   };
 
-  state = {
-        response: '',
-        isHidden: false,
-        videoIsHidden: true,
-        delayIsHidden: true
-    };
-
-  toggleHidden(){
+  toggleSelector(){
       this.setState({
-          isHidden: !this.state.isHidden,
-          // videoIsHidden: !this.state.videoIsHidden
+          selectorIsHidden: !this.state.selectorIsHidden,
       });
   }
 
   startCall(party){
-      this.toggleHidden();
-      console.log("starting call for party: ", party);
-
+      this.toggleSelector();
       connect(party, this.on_delay.bind(this), this.on_call_start.bind(this));
-      // invite(party);
-
-
-
-
-
   };
 
   render() {
@@ -143,7 +92,7 @@ class App extends Component {
           <h1 className="App-title">Conversations</h1>
         </header>
           <div className="user_selection">
-              {!this.state.isHidden && <UserSelection onClick={(party) => this.startCall(party)}/>}
+              {!this.state.selectorIsHidden && <UserSelection onClick={(party) => this.startCall(party)}/>}
           </div>
           <div className="video">
               {!this.state.videoIsHidden && <Video/>}
@@ -157,5 +106,3 @@ class App extends Component {
 }
 
 export default App;
-
-/* END APP CODE */
